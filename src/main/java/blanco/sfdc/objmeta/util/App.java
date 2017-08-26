d1e8ab2f54f5bafacdedad162931ef961bc93278
Toshiki Iga <igapyon@gmail.com>
2017-08-26 18:59:43 +0900

    save

--- src/main/java/blanco/sfdc/objmeta/util/App.java
+++ src/main/java/blanco/sfdc/objmeta/util/App.java
@@ -1,13 +1,15 @@
 package blanco.sfdc.objmeta.util;
 
 /**
- * Hello world!
+ * 
+ * 
+ * mvn archetype:generate -DgroupId=blanco.sfdc.objmeta.util
+ * -DartifactId=blanco-sfdc-objmeta-util
+ * -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
